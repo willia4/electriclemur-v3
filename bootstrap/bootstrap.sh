f4a889764b4a62c496d79a551a67aab9e96bbcdb
James Williams <james@jameswilliams.me>
2019-06-09 11:58:20 -0400

    Allow the --verbose option to be passed from the bootstrap convenience script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 ENVIRONMENT=$1
+VERBOSE=$2
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-"$DIR/dist/bootstrapper.js" create "$ENVIRONMENT"
+if [[ ! -z "$VERBOSE" ]]; then
+  VERBOSE="--verbose"
+fi
+
+"$DIR/dist/bootstrapper.js" create "$ENVIRONMENT" "$VERBOSE"
